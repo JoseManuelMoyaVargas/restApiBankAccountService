85730d80a9da838824a95f16bd7c741bb50a8392
Jose Manuel Moya Vargas <josemanuelmoyavargas@MacBook-Pro-de-Jose.local>
2020-10-20 11:49:17 +0200

    cors available

--- src/main/java/com/restApi_accountService/controller/AccountController.java
+++ src/main/java/com/restApi_accountService/controller/AccountController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.restApi_accountService.services.AccountService;
 
 @RestController
 @RequestMapping("account")
+@CrossOrigin(origins="*")
 public class AccountController {
 	@Autowired
 	private AccountService accountService;
